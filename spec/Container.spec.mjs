aa24d81bc1f8922643946ca0bcd288ca8b44c564
elogeek <elodiechristin@gmail.com>
2022-09-10 10:23:10 +0200

    Minor test with jasmine

--- spec/Container.spec.mjs
+++ spec/Container.spec.mjs
@@ -2,22 +2,22 @@ import {Container} from "../assets/js/components/Container.mjs";
 
 describe("Test de la classe Container", function () {
 
-    const container = new Container();
+    const x = new Container();
     let a;
     let b =["hello", 5];
 
-    it('Container doit être défini, a doit être indéfini', function (){
-        expect(container).toBeDefined();
+    it('x doit être défini, a doit être indéfini', function (){
+        expect(x).toBeDefined();
         expect(a).toBeUndefined();
     });
 
-    it('la const containerr doit contenir la classe Container, la variable b doit contenir hello', function () {
-        expect(container).toContain('Container');
+    it('la const x doit contenir la classe Container, la variable b doit contenir hello', function () {
+        expect(x).toContain('Container');
         expect(b).toContain("hello");
     });
 
-    it('la const container ne doit pas contenir 5, la variable b ne doit pas contenir test', function () {
-        expect(container).toContain(5);
+    it('la const x ne doit pas contenir 5, la variable b ne doit pas contenir test', function () {
+        expect(x).toContain(5);
         expect(b).toContain("test");
     });
 
